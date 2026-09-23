af5a431ede0e8bff31055bc083981339d782766d
Hassan Amiri <hassanamiri.ai@gmail.com>
2026-09-23 08:04:56 +0100

    add example implementation of a singly linked list with three nodes

---
+++ data_structures/linked_list/singly_linkedlist.js
@@ -0,0 +1,44 @@
+/*
+
+Creating an Example Linked List of Size 3 to Understand Working
+Create the first node
+
+Allocate memory for the first node and Store data in it.
+Mark this node as head.
+Create the second node
+
+Allocate memory for the second node and Store data in it.
+Link the first node’s next to this new node.
+Create the third node
+
+Allocate memory for the third node and Store data in it.
+Link the second node’s next to this node.
+Set its next to NULL to ensure that the next of the last is NULL.
+
+
+*/
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+
+  printList() {
+    let current = this;
+    while (current !== null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const box1 = new Node(1);
+
+const box2 = new Node(2);
+
+const box3 = new Node(3);
+
+box1.next = box2;
+box2.next = box3;
+box1.printList();
